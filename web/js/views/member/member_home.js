3a90787a740ae387f0527fb7711fcbc1179f527a
James Milligan <james@milligan.tv>
2015-12-24 14:38:09 +0000

    Styling changes and further prototype for member home

--- web/js/views/member/member_home.js
+++ web/js/views/member/member_home.js
@@ -1,20 +1,26 @@
 define([ 'marionette', 'text!templates/member/member_home.html',
-		'collections/member_collection' ], function(Marionette,
-		memberHomeTemplate, Members) {
+		'collections/member_collection', 'views/member/member_table' ], function(Marionette,
+		memberHomeTemplate, Members, MemberTableView) {
 
 	return Marionette.ItemView.extend({
 		my_template : _.template(memberHomeTemplate),
 		templateHelpers : function() {
 			return {
-				totalMembers : "1596"
+				totalMembers : "1596",
+				overdueMembers: "3",
+				lapsingMembers: "6",
+				openApplications : "20"
 			};
 		},
 
 		initialize : function() {
+			this.render();
 			var that = this;
 			var members = new Members().fetch();
 			members.done(function() {
-				that.render();
+				GtcOffice.show(new MemberTableView({
+					collection: that.members
+				}));
 			});
 		},
 
