65735224f3c0361cf31e3af5b6ead5bd34d2502d
James Milligan <james@milligan.tv>
2016-02-28 19:21:34 +0000

    Move promises around even further

--- office/js/views/dash.js
+++ office/js/views/dash.js
@@ -4,14 +4,6 @@ define([ 'marionette', 'text!templates/dash.html' ], function(Marionette,
 	return Marionette.LayoutView.extend({
 		template : _.template(dashTemplate),
 		templateHelpers : function() {
-			GtcOffice.getProfile().done(function() {
-				if (GtcOffice.isLoggedIn) {
-					GtcOffice.regions.getRegion('header').currentView.render();
-					GtcOffice.showView(new DashView());
-					GtcOffice.setNav("dash");
-				}
-			});
-			
 			return {
 				optionalMessage : this.optionalMessage,
 				loggedIn : GtcOffice.userProfile == null ? false : true,
@@ -20,8 +12,12 @@ define([ 'marionette', 'text!templates/dash.html' ], function(Marionette,
 		},
 
 		initialize : function(options) {
+			var self = this;
 			this.optionalMessage = options.optionalMessage;
-			this.render();
+			GtcOffice.getProfile().done(function() {
+				self.render();
+			});
+			
 		},
 	});
 });
