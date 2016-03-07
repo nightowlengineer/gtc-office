d0e5bc7e6d1ed5029f78a744bd8a1b394fab8b73
James Milligan <james@milligan.tv>
2016-03-07 20:50:51 +0000

    Move typeahead to ensure profile has been loaded

--- office/js/views/header.js
+++ office/js/views/header.js
@@ -60,36 +60,42 @@ define(
 
 						onShow : function() {
 							$('.dropdown-toggle').dropdown();
-							var membersBloodhound = new Bloodhound(
-									{
-										datumTokenizer : Bloodhound.tokenizers.obj
-												.whitespace('value'),
-										queryTokenizer : Bloodhound.tokenizers.whitespace,
-										remote : {
-											url : 'https://services.gtc.org.uk/api/member/search/%memberNumber',
-											wildcard : '%memberNumber',
-											cache : false
-										}
-									});
-							$('#members-typeahead .typeahead')
-									.typeahead(
-											null,
-											{
-												hint : true,
-												highlight : true,
-												minLength : 1,
-												name : 'membersTA',
-												display : 'membershipNumber',
-												source : membersBloodhound,
-												templates : {
-													empty : [
-															'<div class="empty-message">',
-															'Unable to find any members that match the current query',
-															'</div>' ]
-															.join('\n'),
-													suggestion : Handlebars
-															.compile('<div class="statusColor statusColor-{{status}}"><strong>{{membershipNumber}}</strong> {{firstName}} {{lastName}}<br />Type: {{type}}</div>')
-												}
+							GtcOffice
+									.getProfile()
+									.done(
+											function() {
+												var membersBloodhound = new Bloodhound(
+														{
+															datumTokenizer : Bloodhound.tokenizers.obj
+																	.whitespace('value'),
+															queryTokenizer : Bloodhound.tokenizers.whitespace,
+															remote : {
+																url : 'https://services.gtc.org.uk/api/member/search/%memberNumber',
+																wildcard : '%memberNumber',
+																cache : false
+															}
+														});
+												$(
+														'#members-typeahead .typeahead')
+														.typeahead(
+																null,
+																{
+																	hint : true,
+																	highlight : true,
+																	minLength : 1,
+																	name : 'membersTA',
+																	display : 'membershipNumber',
+																	source : membersBloodhound,
+																	templates : {
+																		empty : [
+																				'<div class="empty-message">',
+																				'Unable to find any members that match the current query',
+																				'</div>' ]
+																				.join('\n'),
+																		suggestion : Handlebars
+																				.compile('<div class="statusColor statusColor-{{status}}"><strong>{{membershipNumber}}</strong> {{firstName}} {{lastName}}<br />Type: {{type}}</div>')
+																	}
+																});
 											});
 						}
 					})
