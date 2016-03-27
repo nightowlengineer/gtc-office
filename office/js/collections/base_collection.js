7506e350a6a9aee46f1eadad9ecd61a066a5ceb0
James Milligan <James Milligan>
2016-03-28 00:41:11 +0100

    Add error handler for collection fetch

--- office/js/collections/base_collection.js
+++ office/js/collections/base_collection.js
@@ -10,7 +10,11 @@ define([ 'backbone' ], function(Backbone) {
 			    'Authorization': 'Bearer ' + localStorage.getItem('userToken')
 			  };
 			options.headers = options.headers ? _.extend(options.headers, custom) : custom;
+			options.error = this.errorHandler;
 			return Backbone.Collection.prototype.fetch.call(this, options);
+		},
+		errorHandler : function(collection, response, options) {
+			GtcOffice.navigate("#error/502", true);
 		}
 	});
 
