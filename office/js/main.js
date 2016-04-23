c2025150c7ce425d6c088e69d265e3872942536f
James Milligan <james@milligan.tv>
2016-04-23 22:22:20 +0100

    Tidy up main.js to point to use cdnjs.com where possible

--- office/js/main.js
+++ office/js/main.js
@@ -1,35 +1,33 @@
-// Use https://cdnjs.com to find hosted resources
-
 require
 		.config({
-			baseUrl : '/office/js/',
+			baseUrl : "/office/js/",
 			paths : {
-				jquery : 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min',
-				json2 : 'https://cdnjs.cloudflare.com/ajax/libs/json2/20150503/json2.min',
-				underscore : 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min',
-				'underscore.string' : 'https://cdnjs.cloudflare.com/ajax/libs/underscore.string/3.3.4/underscore.string.min',
-				backbone : 'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min',
-				"backbone-deep-model" : 'libs/backbone-deep-model.min',
-				marionette : 'https://cdnjs.cloudflare.com/ajax/libs/backbone.marionette/2.4.5/backbone.marionette.min',
-				handlebars : 'https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.5/handlebars.amd.min',
-				"jquery-ui" : 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min',
-				text : 'https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min',
-				app : 'app',
-				templates : 'templates',
-				bootstrap : 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min',
-				typeahead : 'libs/typeahead.jquery',
-				bloodhound : 'https://cdnjs.cloudflare.com/ajax/libs/corejs-typeahead/0.11.1/bloodhound.min',
-				datatables : 'https://cdn.datatables.net/1.10.11/js/jquery.dataTables',
-				"x-editable" : 'https://cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.1/bootstrap3-editable/js/bootstrap-editable.min',
-				pace : 'https://cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min',
-				"auth0-lock" : 'https://cdn.auth0.com/js/lock-8.2.min'
+				jquery : "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min",
+				json2 : "https://cdnjs.cloudflare.com/ajax/libs/json2/20150503/json2.min",
+				underscore : "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min",
+				"underscore.string" : "https://cdnjs.cloudflare.com/ajax/libs/underscore.string/3.3.4/underscore.string.min",
+				backbone : "https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min",
+				"backbone-deep-model" : "libs/backbone-deep-model.min",
+				marionette : "https://cdnjs.cloudflare.com/ajax/libs/backbone.marionette/2.4.5/backbone.marionette.min",
+				handlebars : "https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.5/handlebars.amd.min",
+				"jquery-ui" : "https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min",
+				text : "https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min",
+				bootstrap : "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min",
+				typeahead : "https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.11.1/typeahead.jquery.min",
+				bloodhound : "https://cdnjs.cloudflare.com/ajax/libs/corejs-typeahead/0.11.1/bloodhound.min",
+				datatables : "https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.11/js/jquery.dataTables.min",
+				"x-editable" : "https://cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.1/bootstrap3-editable/js/bootstrap-editable.min",
+				pace : "https://cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min",
+				"auth0-lock" : "https://cdn.auth0.com/js/lock-8.2.min",
+				app : "app",
+				templates : "templates",
 			},
 			shim : {
 				underscore : {
 					exports : "_"
 				},
-				'underscore.string' : {
-					deps : ["underscore"],
+				"underscore.string" : {
+					deps : [ "underscore" ],
 					exports : "s"
 				},
 				backbone : {
@@ -44,11 +42,11 @@ require
 					deps : [ "backbone" ],
 					exports : "Marionette"
 				},
-				'jquery-ui' : [ "jquery" ],
+				"jquery-ui" : [ "jquery" ],
 				tpl : [ "text" ],
 				typeahead : {
-					deps : [ 'jquery' ],
-					exports : 'jquery'
+					deps : [ "jquery" ],
+					exports : "jquery"
 				},
 				bootstrap : {
 					deps : [ "jquery" ],
@@ -57,9 +55,9 @@ require
 					deps : [ "jquery" ],
 					exports : "Bloodhound"
 				},
-				'x-editable' : {
-					deps : ["jquery", "bootstrap"],
-					exports : 'jquery'
+				"x-editable" : {
+					deps : [ "jquery", "bootstrap" ],
+					exports : "jquery"
 				}
 			}
 		});
