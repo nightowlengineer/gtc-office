5811291ad404cee9401d4b6bfb543112a4320eaf
James Milligan <james@milligan.tv>
2016-05-01 18:07:40 +0100

    Shift success around slightly

--- office/js/models/member_model.js
+++ office/js/models/member_model.js
@@ -2,32 +2,31 @@ define([ 'models/base_model' ], function(BaseModel) {
 
 	return BaseModel.extend({
 		urlRoot : new BaseModel().urlRoot + "member/id/",
-		
-		createMember : function()
-		{
+
+		createMember : function() {
 			var oldUrl = this.urlRoot;
 			this.urlRoot = new BaseModel().urlRoot + "member/";
-			this.save({
-				success : function(createdMember)
+			this.save(
+				this.attributes,
 				{
-					console.debug(createdMember);
+					success : function(model, response)
+					{
+						console.debug(model);
+					}
 				}
-			});
+			);
 			this.urlRoot = oldUrl;
 		},
-		
-		getSalutations : function(cache)
-		{
+
+		getSalutations : function(cache) {
 			return this.getPlainData("member/salutationTypes", cache);
 		},
-		
-		getLocations : function(cache)
-		{
+
+		getLocations : function(cache) {
 			return this.getPlainData("member/locationTypes", cache);
 		},
-		
-		getStatuses : function(cache)
-		{
+
+		getStatuses : function(cache) {
 			return this.getPlainData("member/statusTypes", cache);
 		}
 	});
